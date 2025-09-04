649d74d1d9468ec198ab85ab4de5044a04b63528
MHussain7860 <malikhussain.cloud@gmail.com>
2025-09-04 07:47:01 +0000

    Revert "updated knl.sh and test.sh"
    This reverts commit d9cee5acffd2971aba62611ae5a32e5ccd13f6b6.

--- knl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo " this is a shell script"
-
-for i in {1..10}
-do
- echo "malik"
-done;
--- test.sh
+++
@@ -1,3 +0,0 @@
-this is just a test file
-
-please check after revert from remote to local
